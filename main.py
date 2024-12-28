dac18437c4e62a3400c8ddb573e55481474c8374
Meera Menon <46912602+meeramenon07@users.noreply.github.com>
2024-12-28 11:39:54 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#kilometers = 0.62137 * miles
+def km_miles():
+  kilometers = float(input(" Enter the value in Kilometers : "))
+  miles = kilometers * 0.62137
+  kilometers = miles /0.62137 
+  print(kilometers, "Kilometers is equal to " , miles, "miles")
+  #return miles
+
+km_miles()
+      
